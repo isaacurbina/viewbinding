a666f5880ccd36b7f6c356578873484cfd67e3e1
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-08-12 11:51:31 -0500

    2. Adding view binding in MainActivity (#1)

--- app/src/main/java/com/iucoding/viewbinding/MainActivity.kt
+++ app/src/main/java/com/iucoding/viewbinding/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.iucoding.viewbinding
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.iucoding.viewbinding.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val mainViewModel: MainViewModel by viewModel<MainViewModel>()
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.submitButton.setOnClickListener {
+            val name = binding.nameInput.text
+            if (name.isNullOrBlank()) {
+                showAlert("Please enter a valid name")
+            } else showAlert("You entered \"$name\"")
+        }
+    }
+
+    private fun showAlert(message: String) {
+        val alertDialog = AlertDialog.Builder(this)
+            .also {
+                it.setMessage(message)
+                it.setPositiveButton("OK") { _: DialogInterface, _: Int -> }
+            }
+            .create()
+            .show()
     }
 }
