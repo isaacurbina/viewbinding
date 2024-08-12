140aee23cc46dbda094de8f0c309d234b02b7109
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-08-12 12:58:29 -0500

    2. Adding recyclerview with view binding

--- app/src/main/java/com/iucoding/viewbinding/di/AppModule.kt
+++ app/src/main/java/com/iucoding/viewbinding/di/AppModule.kt
@@ -1,10 +1,12 @@
 package com.iucoding.viewbinding.di
 
-import com.iucoding.viewbinding.MainViewModel
+import com.iucoding.viewbinding.viewmodel.MainViewModel
+import kotlinx.coroutines.Dispatchers
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
     // ViewModel for MainActivity
     viewModelOf(::MainViewModel)
+    single { Dispatchers.IO }
 }
---
+++ app/src/main/java/com/iucoding/viewbinding/model/HoursItem.kt
@@ -0,0 +1,7 @@
+package com.iucoding.viewbinding.model
+
+data class HoursItem(
+    val name: String,
+    val hours: Int,
+    val country: String
+)
